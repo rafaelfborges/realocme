1db001693d033f14d20d918e95bd2f5480e0c80b
Rafael Borges <rafaelfborges@gmail.com>
2020-06-03 17:44:15 -0300

    Create HomeController actions

--- src/java/controller/HomeController.java
+++ src/java/controller/HomeController.java
@@ -18,16 +18,30 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class HomeController extends HttpServlet {
 
-    
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+        String opcao = request.getParameter("opcao");
+        
+        switch(opcao.trim()){
+            case "cadastrar":
+                request.getRequestDispatcher("pages/cadastro.jsp").forward(request, response);
+                break;
+            
+            case "listar":
+                request.getServletContext().getRequestDispatcher("/ListarUsuarioController").forward(request, response);
+                break;
+                
+            case "editar":
+                request.getServletContext().getRequestDispatcher("/EditarUsuarioController?funcao=opcoes").forward(request, response);
+                break;
+                
+            case "deletar":
+                request.getServletContext().getRequestDispatcher("/DeletarUsuarioController?funcao=opcoes").forward(request, response);
+                break;
+        }
     }
 
 }
